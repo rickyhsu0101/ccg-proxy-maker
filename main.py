7b228ca76941f245dc7c56fdbf29b1826df192cc
Ricky Hsu <rickyhsu0101@gmail.com>
2025-07-17 16:54:52 -0500

    changed background box design

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ from model.document import DocumentType
 from data.read_file import CardListFileReader
 from util.text import CardText
 
+
+# LOCAL_DB: dict[str, str] = {
+#     MKI/W126
+# }
+
 def main():
     # def formatter(series, sets, id):
     #     return f"https://en.ws-tcg.com/wp/wp-content/images/cardimages/n/{series.lower()}_{sets.lower()}/{series.upper()}_{sets.upper()}_{id.upper()}.png"
     
     fr = CardListFileReader()
-    # l = fr.parse_file("deck.txt", "en")
-    l = fr.parse_file_csv("cards.csv", "jp")
+    #l = fr.parse_file_csv("deck.csv", "jp")
+    l = fr.parse_file_csv("deck2.csv", "jp")
     # l = fr.parse_file_local("deck.txt",formatter,"en")
     #print(l)
     #return
@@ -28,7 +33,11 @@ def main():
         int(4*10/3)
     ).set_spacing(
         int(8*10/3)
-    ).set_stroke_width(int(2*10/3))
+    ).set_outline_rect(
+        True
+    ).set_full_background(
+        True
+    ).set_stroke_width(3) #2*10/3 #int(3)
 
     doc: Document = Document().set_dpi(
         1000
@@ -45,6 +54,6 @@ def main():
     # doc: Document = Document().set_dpi(1000).set_card_type(CardType.STANDARD).set_document_type(DocumentType.LETTER).set_padding(30).set_dividers(10,10)
     pages: List[Image.Image] = doc.output_document(imageList= images, cardDataList=l)
 
-    pages[0].save("out5.pdf", save_all = True, append_images = pages[1:]) 
+    pages[0].save("mkichoicebar8.pdf", save_all = True, append_images = pages[1:]) 
 if __name__ == "__main__":
     main()
--- util/text.py
+++ util/text.py
@@ -3,21 +3,29 @@ from PIL import ImageDraw, ImageFont, Image, ImageColor
 from PIL.ImageFont import FreeTypeFont
 from data.model.CardData import CardData, DEFAULT_TEXT_ANCHOR
 from util.model.TextBound import TextBound
-# from data.model.WeisCardColor import WeissCardColor
+from data.model.WeisCardColor import WeissCardColor
 from data.model.WeissCardType import WeissCardType
 class CardText():
     def __init__(self):
         self.texts: List[str] = []
-        self.font: FreeTypeFont = ImageFont.truetype("Arial.ttf", 20)
+        self.font: FreeTypeFont = ImageFont.truetype("DejaVuSans.ttf", 20)
         self.fontSize: int = 20
         self.spacing: int = 8
         self.line_padding: int = 4
         self.stroke_width: int = 2
+        self.outline_rect: bool = False
+        self.full_background: bool = False
+    def set_outline_rect(self, outline_rect):
+        self.outline_rect = outline_rect
+        return self
+    def set_full_background(self, full_background):
+        self.full_background = full_background
+        return self
     def set_texts(self, texts: List[str]):
         self.texts = texts
         return self
     def set_font_size(self, fontSize: int):
-        self.font = ImageFont.truetype("Arial.ttf", fontSize)
+        self.font = ImageFont.truetype("DejaVuSans.ttf", fontSize)
         self.fontSize = fontSize
         return self
     def set_spacing(self, spacing: int):
@@ -97,26 +105,76 @@ class CardText():
                 font = self.font
             )
         )
+        #print(cardColor)
+        
+        color = ImageColor.getrgb(cardColor)
+        if cardColor.lower() == "yellow":
+            color = "orange"
+        if cardColor.lower() == "blue":
+            color = ImageColor.getrgb("#0076c0")
+        if cardColor.lower() == "red":
+            color = ImageColor.getrgb("#b31f1d")
+        # print(color)
+        # print(self.outline_rect)
+        # exit(1)
+
         opacity = 0.7
         draw.rectangle(
             xy = [
-                loc,
                 (
-                    loc[0] + length + 2 * self.line_padding,
-                    loc[1] + self.fontSize + 2 * self.line_padding
+                    loc[0] - (4 if self.outline_rect else 0),
+                    loc[1] - (4 if self.outline_rect else 0)
+                ),
+                (
+                    loc[0] + length + 2 * self.line_padding + (4 if self.outline_rect else 0),
+                    loc[1] + self.fontSize + 2 * self.line_padding + (4 if self.outline_rect else 0)
                 )
             ],
-            fill = ImageColor.getrgb(cardColor) + (int(255*opacity),),
-            width = 0
+            fill = (211,211,211) + (int(255*opacity),),
+            # fill = ImageColor.getrgb(cardColor) + (int(255*opacity),),
+            outline = color if self.outline_rect else None,
+            width = 4 if self.outline_rect else 0
         )
-    def _draw_text(self, draw: ImageDraw.ImageDraw, text: str, loc: Tuple[int, int]):
+    def _draw_full_text_background(self, draw: ImageDraw.ImageDraw, start: Tuple[int, int], end: Tuple[int, int], cardColor: str):
+        opacity = 0.7
+        color = ImageColor.getrgb(cardColor)
+        if cardColor.lower() == "yellow":
+            color = "orange"
+        if cardColor.lower() == "blue":
+            color = ImageColor.getrgb("#006aac")
+        if cardColor.lower() == "red":
+            color = ImageColor.getrgb("#c72321")
+        draw.rectangle(
+            xy = [
+                (
+                    start[0] - (10 if self.outline_rect else 0),
+                    start[1] - (10 if self.outline_rect else 0)
+                ),
+                (
+                    end[0] + (10 if self.outline_rect else 0),
+                    end[1] + (10 if self.outline_rect else 0)
+                ),
+            ],
+            fill = (211,211,211) + (int(255*opacity),),
+            # fill = ImageColor.getrgb(cardColor) + (int(255*opacity),),
+            outline = color if self.outline_rect else None,
+            width = 10 if self.outline_rect else 0
+        )
+    def _draw_text(self, draw: ImageDraw.ImageDraw, text: str, loc: Tuple[int, int], color: WeissCardColor):
+        # fill: str = "white"
+        # stroke: str = "white"
+        # if (color == WeissCardColor.YELLOW):
+        #     fill = "black"
+        #     stroke = "black"
+        fill: str = "black"
+        stroke: str = "black"
         draw.text(
             xy = (loc[0]+self.line_padding, loc[1] + self.line_padding),
             text = text,
             font = self.font,
-            fill= "white",
+            fill= fill,
             stroke_width=self.stroke_width,
-            stroke_fill="black"
+            stroke_fill=stroke
         )
     def _update_start_point(self, point: Tuple[int, int]):
         return (
@@ -153,13 +211,16 @@ class CardText():
         lines: List[str] = self._flatten_multiline_texts(multiline_texts)
 
         startPoint: Tuple[int, int] = (0,0)
+        endPoint: Tuple[int, int] = (0,0)
         if(textBound.topLeft is not None):
             startPoint = textBound.topLeft
+            endPoint = (startPoint[0] + textBound.width, startPoint[1] + height)
         else:
             startPoint = (
                 textBound.bottomRight[0] - textBound.width,
                 textBound.bottomRight[1] - height
             )
+            endPoint = textBound.bottomRight
         point: Tuple[int, int] = startPoint
         print(startPoint)
         print(multiline_texts)
@@ -168,18 +229,26 @@ class CardText():
         print(textBound)
         overlay = Image.new('RGBA', image.size, (255,255,255,0))
         overlayDraw = ImageDraw.Draw(overlay)
-        [
-            (
-                self._draw_text_background(
-                    draw = overlayDraw,
-                    text = line,
-                    loc = point,
-                    cardColor = cardData.cardColor.name
-                ),
-                point := self._update_start_point(point)
+        if self.full_background:
+            self._draw_full_text_background(
+                draw = overlayDraw,
+                start = startPoint,
+                end = endPoint,
+                cardColor = cardData.cardColor.name
             )
-            for line in lines
-        ]
+        else:
+            [
+                (
+                    self._draw_text_background(
+                        draw = overlayDraw,
+                        text = line,
+                        loc = point,
+                        cardColor = cardData.cardColor.name
+                    ),
+                    point := self._update_start_point(point)
+                )
+                for line in lines
+            ]
 
         newImage = Image.alpha_composite(image.convert("RGBA"), overlay)
         newImageDraw = ImageDraw.Draw(newImage)
@@ -189,7 +258,8 @@ class CardText():
                 self._draw_text(
                     draw = newImageDraw,
                     text = line,
-                    loc = point
+                    loc = point,
+                    color = cardData.cardColor
                 ),
                 point := self._update_start_point(point)
             )
